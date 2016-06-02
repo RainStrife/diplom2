f03b962795d7261241ca7c089de5bef6a7186afb
RainStrife <Strife332@yandex.ru>
2016-06-02 20:05:41 +0300

    начало

--- project/urls.py
+++ project/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+import core.urls
+from django.conf.urls.static import static
+
+from project import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^$', 'auth2.views.home'),
-    url(r'^email-sent/', 'auth2.views.validation_sent'),
-    url(r'^login/$', 'auth2.views.home'),
-    url(r'^logout/$', 'auth2.views.logout'),
-    url(r'^done/$', 'auth2.views.done', name='done'),
-    url(r'^accounts/profile/$', 'auth2.views.done', name='done'),
-    url(r'^ajax-auth/(?P<backend>[^/]+)/$', 'auth2.views.ajax_auth',
-        name='ajax-auth'),
-    url(r'^email/$', 'auth2.views.require_email', name='require_email'),
-
+    url(r'', include(core.urls)),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
