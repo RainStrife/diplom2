448e9c2d61d4b80186b884486dfe44991519690c
nick1994209 <nick1994209@yandex.ru>
2016-06-04 17:50:09 +0300

    add ajax and events list for week

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from core import views
 urlpatterns = [
     url(r'^$', views.index, name='index_view'),
     url(r'^contacts/$', views.contact_information, name='contact_information_view'),
+    url(r'^calendar_events/$', views.calendar_events, name='calendar_events'),
     url(r'^video/$', views.video_list, name='video_list_view'),
     url(r'^photo/albums/$', views.photo_album_list, name='photo_album_list_view'),
     url(r'^photo/albums/(?P<pk>[-\d]+)/$', views.photo_album_detail, name='photo_album_detail_view'),
